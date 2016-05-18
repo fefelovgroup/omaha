a7c7fb69a2f84730e3e838009e7d4a90ccff40c9
af <reg@afefelov.ru>
2016-05-18 12:02:14 +0100

    chore (hammer): old version

---
+++ third_party/swapguids.ps1
@@ -0,0 +1,33 @@
+
+## SwapGuid.ps1
+##
+## Reads a file, finds any GUIDs in the file, and swaps them for a NewGUID
+##
+
+Param (
+[Parameter(Mandatory=$True)]
+[ValidateNotNull()]
+$filename
+)
+
+$outputFilename = $filename
+
+$text = [string]::join([environment]::newline, (get-content -path $filename))
+
+$sbNew = new-object system.text.stringBuilder
+
+$pattern = "[a-fA-F0-9]{8}-([a-fA-F0-9]{4}-){3}[a-fA-F0-9]{12}"
+
+$lastStart = 0
+
+$null = ([regex]::matches($text, $pattern) | %{
+    $sbNew.Append($text.Substring($lastStart, $_.Index - $lastStart))
+    $guid = [system.guid]::newguid()
+    $sbNew.Append($guid)
+    $lastStart = $_.Index + $_.Length
+})
+$null = $sbNew.Append($text.Substring($lastStart))
+
+$sbNew.ToString() | out-file -encoding ASCII $outputFilename
+
+Write-Output "Done"
